950ffaa0fc2b859218b912f622adcd229d45a4e0
ChetnaSingh1 <53733136+ChetnaSingh1@users.noreply.github.com>
2019-10-22 20:22:03 +0530

    Update change case.c

--- change case.c
+++ change case.c
@@ -1,1 +1,21 @@
-#include
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[66];
+	int i;
+	printf("Enter string : ");
+		gets(str);
+	for(i=0;str[i]!='\0';i++)
+		{
+			if((str[i]>='a')&&(str[i]<='z'))		//changes lower case to upper case
+				str[i]=str[i]-32;
+			else if((str[i]>='A')&&(str[i]<='Z'))		//changes upper case to lower case
+				str[i]=str[i]+32;
+		}
+	printf("New string is : ");
+		for(i=0;str[i]!='\0';i++)
+			printf("%c",str[i]);
+	return 0;
+}
